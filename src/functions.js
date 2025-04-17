14940c060fb7e9a7f705749db33ad93a355a2cc1
MCapredoni <144966520+MCapredoni@users.noreply.github.com>
2025-04-17 01:11:55 +0000

    Q3 Done

--- src/functions.js
+++ src/functions.js
@@ -9,30 +9,30 @@ function colour_mix(rgb_colour1, rgb_colour2){
 
 function largest_product(val1, val2, val3){
     const value = (val1, val2, val3)
-    const
+    
     return value;
 }
 
 function day_of_the_week(day_num){
-        if(day_num = 1){
+        if(day_num == 1){
         return "Sunday";
         }
-        if(day_num = 2){
+        if(day_num == 2){
         return "Monday";
         }
-        if(day_num = 3){
+        if(day_num == 3){
         return "Tuesday";
         }
-        if(day_num = 4){
+        if(day_num == 4){
         return "Wednesday";
         }
-        if(day_num = 5){
+        if(day_num == 5){
         return "Thursday";
         }
-        if(day_num = 6){
+        if(day_num == 6){
         return "Friday";
         }
-        if(day_num = 7){
+        if(day_num == 7){
         return "Saturday";
         }
     
